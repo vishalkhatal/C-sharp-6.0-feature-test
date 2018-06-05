2d510f087942603dcc6bfbf907deca7da44541d2
Vishal Khatal <vishal.khatal2206@gmail.com>
2018-06-05 12:26:46 +0530

    added src code

---
+++ null operation test/null operation test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace null_operation_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var image = new Image();
+            var result = image?.ImagePath ?? string.Empty; // if image is null,it is assinging string.empty
+            Console.Write(result);
+            image.ImagePath = "SampleImage";
+            result = image?.ImagePath ?? string.Empty; // assinging imagePath value
+            Console.Write(result);
+            Console.ReadKey();
+        }
+    }
+    class Image
+    {
+        public string ImagePath { get; set; }
+    }
+}
